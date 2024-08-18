de89659931c1ae2e6fedef98f5eb4b7de7bb483b
smox3y <smokeyboy181@gmail.com>
2024-08-18 18:23:24 -0500

    x

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,15 +22,7 @@ class Footer extends Component {
             <div className="twelve columns">
               <ul className="social-links">{networks}</ul>
 
-              <ul className="copyright">
-                <li>&copy; Copyright 2021 Nordic Giant</li>
-                <li>
-                  Design by{" "}
-                  <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
-                  </a>
-                </li>
-              </ul>
+              
             </div>
           </Fade>
 
